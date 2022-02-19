a8ee29bbb6eb2cae7f3815c0041e7c84c748c19e
sushil-mahto <sushilkr561998@gmail.com>
2022-02-19 13:54:28 +0530

    Initial Commit

---
+++ MyApp/MyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAppApp.swift
+//  MyApp
+//
+//  Created by SUshil on 19/02/22.
+//
+
+import SwiftUI
+
+@main
+struct MyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
